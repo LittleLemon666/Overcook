6cb6e596ffd82e9e06ab4798756e4b9a8b0f541c
Lemon666 <damoncho510@gmail.com>
2021-01-05 21:44:04 +0800

    comment up
    comment up on advance.v

--- lab1/advance.v
+++ lab1/advance.v
@@ -1,12 +1,12 @@
 module advance(output reg [7:0] seg7Out, out reg [3:0] lighting, output endEnable,
 				input clk, input reset, input [3:0] change, input [2:0] buttom, input enable);
 	
-	reg [9:0] counter;
-	reg [1:0] state;
-	reg [7:0] level;
-	reg [3:0] lightsBin [6:0];
-	reg [2:0] lightIndex;
-	parameter LIGHT = 2'b00, APPLY = 2'b01, GOOD = 2'b10, END = 2'b11;
+	reg [9:0] counter; //for divide clk
+	reg [1:0] state; //state in advance.v
+	reg [7:0] level; //your level now
+	reg [3:0] lightsBin [6:0]; //store what LEDs light
+	reg [2:0] lightIndex; //index of LEDs cache
+	parameter LIGHT = 2'b00, APPLY = 2'b01, GOOD = 2'b10, END = 2'b11; //state in advance.v
 	parameter GOODSHOW = 255;
 	parameter ENDSHOW = 253;
 	parameter counter_max = 1000; // 0.5s
@@ -16,28 +16,26 @@ module advance(output reg [7:0] seg7Out, out reg [3:0] lighting, output endEnabl
 	begin
 		if(reset)
 		begin
-			clk_out <= 0;
 			counter <= counter_max;
-			level <= 1;
-			seg7Out <= level;
-			assign endEnable = 0;
-			lightIndex <= 0;
+			level <= 1; //level is 1 in the beginning
+			seg7Out <= level; //seg7 show level
+			assign endEnable = 0; //lock END.v
+			lightIndex <= 0; //reset leds cache
 		end
 		if (counter == 0) //action
 		begin
 			counter <= counter_max;
-			clk_out <= ~clk_out;
 			case(state)
 				LIGHT:  //lighting
 				begin
-					lightsBin[lightIndex] <= $random % 10;
-					lighting <= lightsBin[lightIndex];
-					lightIndex <= lightIndex + 1;
-					seg7Out <= level;
-					if (lightIndex == lightMax)
+					lightsBin[lightIndex] <= $random % 10; //random the index of light
+					lighting <= lightsBin[lightIndex]; //LEDs light the cache "lightsBin[lightIndex]"
+					lightIndex <= lightIndex + 1; //LEDs cache index++
+					seg7Out <= level; //show your level now
+					if (lightIndex == lightMax) //after 7 of lights
 					begin
-						lightIndex <= 0;
-						state <= APPLY;
+						lightIndex <= 0; //from cache 0
+						state <= APPLY; //start to recieve player sw
 					end
 				end
 				
@@ -47,17 +45,17 @@ module advance(output reg [7:0] seg7Out, out reg [3:0] lighting, output endEnabl
 					begin
 						if (change == lightsBin[lightIndex])
 						begin
-							lightIndex <= lightIndex + 1;
+							lightIndex <= lightIndex + 1; //correct!
 						end
 						else
 						begin
-							state <= END;
+							state <= END; //see you next time
 						end
 					end
 					
-					if (lightIndex == lightMax)
+					if (lightIndex == lightMax) //all correct!
 					begin
-						lightIndex <= 0;
+						lightIndex <= 0; //reset leds cache
 						state <= GOOD;
 					end
 				end
@@ -65,15 +63,15 @@ module advance(output reg [7:0] seg7Out, out reg [3:0] lighting, output endEnabl
 				GOOD:  //show GOOD
 				begin
 					state <= LIGHT;
-					level <= level + 1;
-					seg7Out <= GOODSHOW;
-					lightIndex <= 0;
+					level <= level + 1; //level up
+					seg7Out <= GOODSHOW; //show GOOD on seg7
+					lightIndex <= 0;  //reset leds cache
 				end
 				
 				END:  //show END and
 				begin
-					seg7Out <= ENDSHOW;
-					assign endEnable = 1;
+					seg7Out <= ENDSHOW; //show END on seg7
+					assign endEnable = 1; //enable END.v
 				end
 				
 				default:
