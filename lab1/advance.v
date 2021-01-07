e8c4309141bb9d7b85dcba87b8e4cfeaa3747da7
skytanin <55647786+skytanin@users.noreply.github.com>
2021-01-07 11:26:39 +0800

    advance_FU

--- lab1/advance.v
+++ lab1/advance.v
@@ -1,5 +1,4 @@
-module advance(output reg [7:0] seg7Out, output reg [3:0] lighting, output reg endEnable,
-				input clk, input [3:0] change, input enable);
+module advance(output reg [7:0] seg7Out, output reg [3:0] lighting, output reg endEnable,input clk, input [3:0] change, input enable);
 	
 	reg [9:0] counter; //for divide clk
 	reg [1:0] state; //state in advance.v
@@ -22,77 +21,81 @@ module advance(output reg [7:0] seg7Out, output reg [3:0] lighting, output reg e
 	// total sequences (maximum) : 2^3 - 1 = 7
 	assign feedback_value = r_reg[3] ^ r_reg[2] ^ r_reg[0]; //for random
 	assign r_next = {feedback_value, r_reg[3:1]}; //for random
-	 
-	always@(posedge enable)
-	begin
-		counter <= counter_max;
-		level <= 1; //level is 1 in the beginning
-		seg7Out <= level; //seg7 show level
-		endEnable <= 0; //lock END.v
-		lightIndex <= 0; //reset leds cache
-	end
 	
 	always@(posedge clk)
 	begin
-		if (counter == 0) //action
+		if(enable == 0)
 		begin
 			counter <= counter_max;
-			case(state)
-				LIGHT:  //lighting
-				begin
-					r_reg <= r_next; //random the index of light
-					lightsBin[lightIndex] <= r_reg;
-					lighting <= lightsBin[lightIndex]; //LEDs light the cache "lightsBin[lightIndex]"
-					lightIndex <= lightIndex + 1; //LEDs cache index++
-					seg7Out <= level; //show your level now
-					if (lightIndex == lightMax) //after 7 of lights
+			level <= 1; //level is 1 in the beginning
+			seg7Out <= level; //seg7 show level
+			endEnable <= 0; //lock END.v
+			lightIndex <= 0; //reset leds cache
+		end
+		else
+		begin
+			if (counter == 0) //action
+			begin
+				counter <= counter_max;
+				case(state)
+					LIGHT:  //lighting
 					begin
-						lightIndex <= 0; //from cache 0
-						state <= APPLY; //start to recieve player sw
-					end
-				end
-				
-				APPLY:  //input what sw be changed
-				begin
-					if (change != 4'b1111) //recieve SWs is not default
-					begin
-						if (change == lightsBin[lightIndex])
+						r_reg <= r_next; //random the index of light
+						lightsBin[lightIndex] <= r_reg;
+						lighting <= lightsBin[lightIndex]; //LEDs light the cache "lightsBin[lightIndex]"
+						lightIndex <= lightIndex + 1; //LEDs cache index++
+						seg7Out <= level; //show your level now
+						if (lightIndex == lightMax) //after 7 of lights
 						begin
-							lightIndex <= lightIndex + 1; //correct!
-						end
-						else
-						begin
-							state <= END; //see you next time
+							lightIndex <= 0; //from cache 0
+							state <= APPLY; //start to recieve player sw
 						end
 					end
 					
-					if (lightIndex == lightMax) //all correct!
+					APPLY:  //input what sw be changed
 					begin
-						lightIndex <= 0; //reset leds cache
-						state <= GOOD;
+						if (change != 4'b1111) //recieve SWs is not default
+						begin
+							if (change == lightsBin[lightIndex])
+							begin
+								lightIndex <= lightIndex + 1; //correct!
+							end
+							else
+							begin
+								state <= END; //see you next time
+							end
+						end
+						
+						if (lightIndex == lightMax) //all correct!
+						begin
+							lightIndex <= 0; //reset leds cache
+							state <= GOOD;
+						end
 					end
-				end
-				
-				GOOD:  //show GOOD
-				begin
-					state <= LIGHT;
-					level <= level + 1; //level up
-					seg7Out <= GOODSHOW; //show GOOD on seg7
-					lightIndex <= 0;  //reset leds cache
-				end
-				
-				END:  //show END and
-				begin
-					seg7Out <= ENDSHOW; //show END on seg7
-					endEnable <= 1; //enable END.v
-				end
-				
-				default:
-					state <= 0;
-			endcase
+					
+					GOOD:  //show GOOD
+					begin
+						state <= LIGHT;
+						level <= level + 1; //level up
+						seg7Out <= GOODSHOW; //show GOOD on seg7
+						lightIndex <= 0;  //reset leds cache
+					end
+					
+					END:  //show END and
+					begin
+						seg7Out <= ENDSHOW; //show END on seg7
+						endEnable <= 1; //enable END.v
+					end
+					
+					default:
+						state <= 0;
+				endcase
+			end
+			else
+			begin
+				counter <= counter - 1;
+			end
 		end
-		else
-			counter <= counter - 1;
 	end
 	
 endmodule
